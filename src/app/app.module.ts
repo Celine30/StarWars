ebefbd7ae1743d553dcfbb9958cdf19241fc97fb
Celine <celi.caurier@gmail.com>
2020-04-07 11:54:03 +0200

    Create Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiculesComponent } from './vehicules/vehicules.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { FilmsComponent } from './films/films.component';
+import { PeopleComponent } from './people/people.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { SpeciesComponent } from './species/species.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiculesComponent,
+    PlanetsComponent,
+    FilmsComponent,
+    PeopleComponent,
+    StarshipsComponent,
+    SpeciesComponent,
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
