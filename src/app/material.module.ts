f1d42612fe292a17de0d3b446e54d63ea78a2e33
Celine <celi.caurier@gmail.com>
2020-04-14 21:23:35 +0200

    Material.io

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,17 @@
 import {NgModule} from '@angular/core';
 import {MdcButtonModule} from '@angular-mdc/web';
+import {MdcCardModule} from '@angular-mdc/web';
+import {MdcIconModule} from '@angular-mdc/web';
+import {MdcIconButtonModule} from '@angular-mdc/web';
+
+
 
 @NgModule({
     exports: [
         MdcButtonModule,
+        MdcCardModule,
+        MdcIconModule,
+        MdcIconButtonModule
     ]
 })
 
---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,20 @@
+import { Input,Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent implements OnInit {
+
+  @Input() movieTitle: string;
+  @Input() moviePoster: string;
+  @Input() movieDate: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
