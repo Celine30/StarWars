988f82d75cbcdb9aa1c4bed04adcba265babced7
Celine <celi.caurier@gmail.com>
2020-04-14 21:24:15 +0200

    component useful

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from "@angular/router";
+/* import { HttpClientModule } from '@angular/common/http'; */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VehiculesComponent } from './vehicules/vehicules.component';
-import { PlanetsComponent } from './planets/planets.component';
 import { FilmsComponent } from './films/films.component';
-import { PeopleComponent } from './people/people.component';
-import { StarshipsComponent } from './starships/starships.component';
-import { SpeciesComponent } from './species/species.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { MovieComponent } from './movie/movie.component';
 
 import { FilmsService } from './services/films.service';
 
 import {MaterialModule} from './material.module';
 
-import { Routes, RouterModule} from "@angular/router";
 
 const appRoutes : Routes = [
   { path:'films', component : FilmsComponent },
-  { path:'people', component : PeopleComponent },
-  { path:'planets', component : PlanetsComponent },
   { path:'', component : FilmsComponent },
   { path:'not-found', component : ErrorPageComponent },
   { path:'**', redirectTo : '/not-found' },
@@ -31,21 +26,18 @@ const appRoutes : Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    VehiculesComponent,
-    PlanetsComponent,
     FilmsComponent,
-    PeopleComponent,
-    StarshipsComponent,
-    SpeciesComponent,
     HomeComponent,
     HeaderComponent,
-    ErrorPageComponent
+    ErrorPageComponent,
+    MovieComponent
   ],
   imports: [
+ /*    HttpClientModule, */
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     FilmsService
