30b2b35b6f4dab5801407876bd99eabe5c56359f
Celine <celi.caurier@gmail.com>
2020-04-19 20:40:32 +0200

    Obj  Movies  transformed
     information gathered from a film

--- src/app/services/films.service.ts
+++ src/app/services/films.service.ts
@@ -1,35 +1,86 @@
 
 import { Subject } from 'rxjs';
 import { MdcOverline } from '@angular-mdc/web';
+import { Movie} from '../movie'
 
 import { Injectable } from '@angular/core';
-/* import { HttpClient } from '@angular/common/http'; */
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable()
 
 export class FilmsService {
 
-    private movie:any[]
+    private movies=[]
+
+    private id_popular=[]
 
     filmsSubject = new Subject<any[]>();
 
-   /*  constructor(private httpClient: HttpClient) { } */
-
     fetch_movies_popular(){
-
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=da1bce919075394c3d45dccb32acf33e&language=fr&page=1')
         .then(response => {
             return response.json()
         }) .then(json => {
-            this.movie = json.results
-            console.log(this.movie)
-            this.emitFilmsSubject();
+            this.search_Movies(this.check_id(json.results))
         })
     }
 
-    
+    check_id(datas:any){
+        for (const data of datas) {
+            this.id_popular.push(data.id)   
+            }
+        return this.id_popular
+    }
+
+    search_Movies(idMovies:any){
+        for (const idMovie of idMovies) {
+            fetch('https://api.themoviedb.org/3/movie/'+ idMovie +'?api_key=da1bce919075394c3d45dccb32acf33e&language=FR')
+            .then(response => {
+                return response.json()
+            })
+            .then(json => {
+                this.add(json)
+            })
+        }
+    }
+
+    add(details:any){
+        this.movies.push(details)
+        if(this.movies.length == 20){
+            console.log('il y sont tous')
+            for (let i=0 ; i<20 ; i++){
+                    fetch('https://api.themoviedb.org/3/movie/'+ this.id_popular[i] +'/credits?api_key=da1bce919075394c3d45dccb32acf33e')
+                    .then(response => {
+                        return response.json()
+                    })
+                    .then(json => {
+                        this.addB(i ,json)
+                    })
+                }
+            }
+        }
+
+    addB(id: number,details: any){
+        Object.defineProperty(this.movies[id], 'credits', {
+            value: details
+        });
+        if(id==19){
+            console.log('fini')
+            this.emitFilmsSubject();
+        }
+    }
+
     emitFilmsSubject() {
-        this.filmsSubject.next(this.movie.slice());
+        this.filmsSubject.next(this.movies.slice());
     }
 
 }
+
+/* trouver les credits */
+/* https://api.themoviedb.org/3/movie/419704/credits?api_key=da1bce919075394c3d45dccb32acf33e */
+
+/* LISTE DES GENRS */
+/* https://api.themoviedb.org/3/genre/movie/list?api_key=da1bce919075394c3d45dccb32acf33e&language=FR */
+
+/* DETAILS FILMS */
+/* https://api.themoviedb.org/3/movie/419704?api_key=da1bce919075394c3d45dccb32acf33e&language=FR */
